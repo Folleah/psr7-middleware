09f3f4388a149f0776ec491f5966caae7a03afb2
Андрей Волков <folleah5@gmail.com>
2017-06-09 17:21:20 +0400

    Added interfaces

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
 
-    $app = new Application();
+    require __DIR__.'/vendor/autoload.php';
 
-    $app->run();
+    echo 'test';
